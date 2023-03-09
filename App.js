3423e0fb7988527b6dcc48d76d020172f4ec70c2
Wilfried Bertrand <bertdelaspeed@gmail.com>
2023-03-09 18:54:56 +0000

    starting point

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import LoginScreen from "./src/Screens/LoginScreen";
+import RegisterScreen from "./src/Screens/RegisterScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      {/* <LoginScreen /> */}
+      <RegisterScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Screens/LoginScreen.js
@@ -0,0 +1,64 @@
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+const backImage = require("../../assets/background_signin.jpg");
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // console.log("password = ", password);
+
+  return (
+    <View className="bg-black">
+      <View>
+        <Image source={backImage} className="object-cover h-80 w-full" />
+      </View>
+      <View className="bg-white h-screen rounded-t-3xl">
+        <Text className="text-[#d60e45] text-3xl font-semibold text-center py-3 mt-3">
+          Se connecter
+        </Text>
+        <View className="mt-10 items-center">
+          <TextInput
+            className="tracking-widest bg-gray-100 rounded-lg w-80 text-base py-2 px-1 mx-5 mb-5"
+            placeholder="Entrer email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            className="tracking-widest bg-gray-100 rounded-lg w-80 text-base py-2 px-1 mx-5 mb-5"
+            placeholder="Entrer mot de passe"
+            autoCapitalize="none"
+            secureTextEntry={true}
+            autoCorrect={false}
+            textContentType="password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <TouchableOpacity className="bg-[#fac25a] py-2 rounded-md mx-10 mt-10 mb-3">
+          <Text className="text-center font-semibold text-white text-lg">
+            Se connecter
+          </Text>
+        </TouchableOpacity>
+        <View className="flex-row space-x-2 justify-center">
+          <Text className="font-light tracking-wider">Nouveau ici ?</Text>
+          <TouchableOpacity>
+            <Text className="font-medium text-[#d60e45]">Register</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
